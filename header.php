83fb136d95479ec03e705568e0b9f71e0f57ab91
David Ruiz <luisdavid090801@gmail.com>
2026-09-02 19:56:05 -0600

    Diseño CSS completado para Header, Footer y Home Page

--- header.php
+++ header.php
@@ -8,11 +8,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $marca ?></title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
     <header>
         <nav>
-            <a href="index.php" > Inicio </a>
+            <a href="home.php" > Inicio </a>
             <a href="catalogo.php" > Catalogo </a>
             <a href="nosotros.php" > Nosotros </a>
             <a href="perfil.php" > Perfil </a>
--- home.php
+++ home.php
@@ -3,24 +3,33 @@
 ?>
 
 <main>
-    <h1>Maximiza el rendimiento de tu PC</h1>
-    <p>En Verum somos especialistas en ensambles a la medida y mantenimiento profundo.
-        Desde una limpieza detallada y cambio de pasta térmica, hasta una gestión de cables
-        impecable para que tu equipo luzca y funcione como nuevo.
-    </p>
-    <h2>Servicio Destacado</h2>
-    <div>
-        <div style="width: 300px; height: 200px; background-color: #e0e0e0; border: 1px dashed #999;">[Imagen Próximamente]</div>
+    <div class="hero-verum">
+        <h1>Maximiza el rendimiento de tu PC</h1>
+        <p>En Verum somos especialistas en ensambles a la medida y mantenimiento profundo.
+            Desde una limpieza detallada y cambio de pasta térmica, hasta una gestión de cables
+            impecable para que tu equipo luzca y funcione como nuevo.
+        </p>
+    </div>
+
+    <h2 class="centrar-contenido">Servicio Destacado</h2>
+    <div class="tarjeta-fondo-verum">
+        <img src="https://placehold.co/500x250/333333/00d2ff?text=Ensamble+Verum" alt="Servicio Estelar" width="100%">
+        <br><br>
         <h3>Ensamble Gamer Custom</h3>
-        <p>Deja tu equipo en manos expertas. Incluye limpieza profunda,
+        <p class="texto-descripcion">
+            Deja tu equipo en manos expertas. Incluye limpieza profunda,
             gestión de cables impecable y optimización de rendimiento</p>
+        <p class="precio-azul">Desde $800 MXN</p>
+        <br>
         <a href="detalle.php">
-            <button type="button">Ver Detalles</button>
+            <button type="button" class="boton-verum">Ver Detalles</button>
         </a>
     </div>
-    <a href= "catalogo.php" >
-        <button type="button">Ver Catalogo Completo</button>
-    </a>
+    <div class="centrar-contenido">            
+        <a href= "catalogo.php" >
+            <button type="button" class="boton-verum">Ver Catalogo Completo</button>
+        </a>
+    </div>
 </main>
 
 <?php
